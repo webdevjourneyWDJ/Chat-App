7f3524affe9fc0233cf262373ca59c48af31e6f8
Gamaliel Barron <gbarronmm@gmail.com>
2019-01-04 14:35:32 -0800

    added connect and disconnect eevnt listeners

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection', (socket) => {
+  console.log("A new user just connected");
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, ()=>{
   console.log(`Server is up on port ${port}`);
 })
